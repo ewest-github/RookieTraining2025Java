9248759b26cc83c93827a6118460ff0c86519c47
wyne241 <wyne@ewest.co.jp>
2025-03-06 14:52:24 +0900

    Add files via upload

--- 成果物/Wyne/hello/src/main/java/Hello/Hello.java
+++ 成果物/Wyne/hello/src/main/java/Hello/Hello.java
@@ -1,6 +1,7 @@
 package Hello;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,36 +9,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Hello
- */
+
 @WebServlet("/Hello")
 public class Hello extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    
     public Hello() {
         super();
-        // TODO Auto-generated constructor stub
+        
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
 		
-		response.getWriter().println("<html><body><h1>Hello,World!</h1></body></html>");
+		PrintWriter out=response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>");
+		out.println("Hello,World!");
+		out.println("</h1>");
+		out.println("</body>");
+		out.println("</html>");
+		
+		
 		}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
 
 }
