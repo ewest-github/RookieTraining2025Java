05c59112721d6312d649c8c32e9a9053db987b57
wyne241 <wyne@ewest.co.jp>
2025-03-11 10:35:41 +0900

    Add files via upload

--- 成果物/Wyne/hello/src/main/java/Hello/Hello.java
+++ 成果物/Wyne/hello/src/main/java/Hello/Hello.java
@@ -4,27 +4,24 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/Hello")
-public class Hello extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    
-    public Hello() {
-        super();
-        
-    }
-
-	
+//HelloクラスはHttpServletを継承する
+public class Hello extends HttpServlet 
+{ 
+   //GETリクエストを処理するメソッド
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//クライアントに送信するレスポンスのコンテンツタイプを設定する
 		response.setContentType("text/html;charset=UTF-8");
 		
+		//レスポンスを出力するためのPrintWriterのオブジェクトを取得する
 		PrintWriter out=response.getWriter();
+		
+		//Htmlのコンテンツの出力
 		out.println("<html>");
 		out.println("<body>");
 		out.println("<h1>");
